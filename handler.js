9607672b5182d0db23303ccae7e63ca4ac37672d
Fran Ortiz <fran.ortiz@nextail.co>
2018-11-06 22:01:58 +0100

    Add check to fail when the body does not have the proper format

--- handler.js
+++ handler.js
@@ -2,15 +2,8 @@
 
 const github = require('octonode')
 
-module.exports.githubCheck = (event, context, callback) => {
-  const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET
-  const githubToken = process.env.GITHUB_TOKEN
-  const github_client = github.client(githubToken)
-  let response = {
-    statusCode: 204
-  }
-
-  if (typeof webhookSecret !== 'string') {
+const checkWebhookSecret = (secret) => {
+  if (typeof secret !== 'string') {
     response = {
       statusCode: 401,
       headers: { 'Content-Type': 'text/plain' },
@@ -18,13 +11,22 @@ module.exports.githubCheck = (event, context, callback) => {
     }
     return callback(null, response);
   }
+}
 
+module.exports.githubCheck = (event, context, callback) => {
+  const webhookSecret = process.env.GITHUB_WEBHOOK_SECRET
+  const githubToken = process.env.GITHUB_TOKEN
+  const github_client = github.client(githubToken)
+
+  checkWebhookSecret(webhookSecret)
   let body = JSON.parse(event.body)
+  let response = { statusCode: 204 }
   /* eslint-disable */
   console.log('---------------------------------');
-  console.log('Input body', body);
+  console.log('Input body', event.body);
   /* eslint-enable */
   if (!body || !body.hasOwnProperty('pull_request')) {
+    response['body'] = 'Event is not a Pull request'
     return callback(null, response)
   }
 
@@ -33,6 +35,12 @@ module.exports.githubCheck = (event, context, callback) => {
     description: 'PR title according to format',
     context: 'serverless-webhook/pr-title'
   }
+
+  if (!body.pull_request.body.startsWith('Related to: https://trello.com')) {
+    payload.state = 'failure'
+    payload.description = 'PR title should start with a ticket number';
+  }
+
   github_client.post(`/repos/${body.repository.full_name}/statuses/${body.pull_request.head.sha}`, payload, function () {
     callback(null, response)
   })
